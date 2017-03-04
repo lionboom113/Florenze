58a3e8d1f2619be74e50b749909138ee251df603
Tuan Huynh <huynhminhanhtuan@gmail.com>
2017-03-05 02:37:38 +0700

    event button for fragment

--- app/src/main/java/com/codepath/apps/restclienttemplate/Fragment/PostTweetFragment.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/Fragment/PostTweetFragment.java
@@ -2,11 +2,14 @@ package com.codepath.apps.restclienttemplate.Fragment;
 
 
 import android.app.DialogFragment;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.codepath.apps.restclienttemplate.R;
@@ -17,6 +20,8 @@ import butterknife.ButterKnife;
 public class PostTweetFragment extends DialogFragment {
     @BindView(R.id.etPostTweet)
     EditText etPostTweet;
+    @BindView(R.id.btnPostTweet)
+    Button btnPostTweet;
 
     public PostTweetFragment() {
         // Empty constructor is required for DialogFragment
@@ -40,11 +45,22 @@ public class PostTweetFragment extends DialogFragment {
         return view;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // Show soft keyboard automatically and request focus to field
         etPostTweet.requestFocus();
+        btnPostTweet.setOnContextClickListener(new View.OnContextClickListener() {
+            @Override
+            public boolean onContextClick(View v) {
+                etPostTweet.setText("Heowd");
+                return false;
+            }
+        });
+
+    }
+    public void postTweet(View view){
 
     }
 
